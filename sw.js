d9a9a9d97ca54abbf04d6d634acd2fd0661bb1d3
sbvorpagel <sbvorpagel@gmail.com>
2020-08-11 17:29:33 -0400

    Create the minimal PWA

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open('pwa-cache')
+			.then(function(cache) {
+				return cache.add('index.html');
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.repondWith(
+		caches
+			.match(event.request)
+			.then(function() {
+				return response || fetch(event.request);
+			})
+	);
+});
